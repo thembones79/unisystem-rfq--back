1db1d4e8cc9f5ee4424c98fa2a224f13f85393c0
thembones79 <michal.szumnarski@gmail.com>
2022-04-20 17:52:02 +0200

    ### 🔥 Added
    - `project` ClickUp integration
    - `project` SharePoint integration

--- src/routes/projects/new.ts
+++ src/routes/projects/new.ts
@@ -1,16 +1,17 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
-
+import { spFileCreate } from "../../services/spFileCreate";
+import { ClickUp } from "../../services/clickup";
 import { validateRequest, requireAuth } from "../../middlewares";
 import { ProjectRepo } from "../../repos/project-repo";
 import { ProjectClientRepo } from "../../repos/project-client-repo";
 import { concatZeros } from "../../services/concatZeros";
+import { UserRepo } from "../../repos/user-repo";
+import { BadRequestError } from "../../errors";
 
 const router = express.Router();
 
-const newProjectCode = async (clientId: string) => {
-  const client = await ProjectClientRepo.findById(clientId);
-  const clientCode = client.code as string;
+const newProjectCode = async (clientCode: string) => {
   const lastNumber = await ProjectRepo.findMaxNumberForGivenCode(clientCode);
   return clientCode + concatZeros(lastNumber + 1 + "", 3);
 };
@@ -39,23 +40,39 @@ router.post(
       .notEmpty()
       .isNumeric()
       .withMessage("You must supply a RfqId"),
-    body("department")
-      .notEmpty()
-      .trim()
-      .withMessage("You must supply a Department"),
     body("note").trim(),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    const { project_client_id, industry_id, department, pm_id, note, rfq_id } =
-      req.body;
+    const { project_client_id, industry_id, pm_id, note, rfq_id } = req.body;
 
-    const project_code = await newProjectCode(project_client_id);
+    const pm = await UserRepo.findById(pm_id);
+    if (!pm) {
+      throw new BadRequestError("PM does not exist");
+    }
 
-    const clickup_id = "";
+    const pmEmail = pm.email as string;
+
+    const client = await ProjectClientRepo.findById(project_client_id);
+    const clientCode = client.code as string;
+    const clientName = client.name as string;
+    const clientKam = client.kam as string;
+    const list = client.client_list_clickup_id as string;
+    const clientKamAlt = client.kam_alt as string;
+    const clientKamRole = client.kam_role as number;
+    const project_code = await newProjectCode(clientCode);
+
+    const clickup_id = await ClickUp.createTask({
+      pmEmail,
+      code: `[${project_code}]`,
+      status: "open",
+      list,
+    });
+
     const version = "";
     const revision = "";
-    console.log({ project_client_id });
+    const department = clientKamRole === 4 ? "EX" : "PL";
+
     const newProject = await ProjectRepo.insert({
       project_code,
       project_client_id,
@@ -67,6 +84,13 @@ router.post(
       clickup_id,
       version,
       revision,
+    });
+
+    await spFileCreate({
+      projectCode: project_code,
+      department,
+      clientName,
+      kam: clientKamAlt,
     });
 
     res.status(201).send(newProject);
--- src/routes/projects/show.ts
+++ src/routes/projects/show.ts
@@ -1,16 +1,29 @@
 import express from "express";
 import { requireAuth } from "../../middlewares";
 import { ProjectRepo } from "../../repos/project-repo";
+import { ClickUp } from "../../services/clickup";
 import { NotFoundError } from "../../errors";
 
 const router = express.Router();
 
 router.get("/projects/:id", requireAuth, async (req, res) => {
   const { id } = req.params;
-  const project = await ProjectRepo.findById(id);
+  let project = await ProjectRepo.findById(id);
 
   if (!project) {
     throw new NotFoundError();
+  }
+
+  let status = "task not found";
+
+  try {
+    status = await ClickUp.getTaskStatus(project.clickup_id);
+  } catch (error: any) {
+    console.error({ error });
+  }
+
+  if (status) {
+    project = { ...project, status: status.toLowerCase() };
   }
 
   res.send(project);
